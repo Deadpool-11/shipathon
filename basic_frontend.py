a38b2b839c1b810756c17cb3e55e131075c772c3
Deadpool-11 <cs1240177@iitd.ac.in>
2025-01-12 02:28:14 +0530

    Update basic_frontend.py
    ..

--- basic_frontend.py
+++ basic_frontend.py
@@ -3,11 +3,11 @@ import altair as alt
 import pandas as pd
 st.set_page_config(page_title="EventConnect", page_icon="📅", layout="wide")
 
-"""
-input: csv1(priority sorted) is df1 and csv2(time sorted) is df2 and d
-output: calendar
-extra need: all csv time in that format..... 24 hour format
-"""
+# """
+# input: csv1(priority sorted) is df1 and csv2(time sorted) is df2 and d
+# output: calendar
+# extra need: all csv time in that format..... 24 hour format
+# """
 
 
 
