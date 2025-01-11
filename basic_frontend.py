a9390084f7a9a0c78a0884088a4e98501b8d98f8
Sambhav Singh Aditya <sambhavsinghaditya@Sambhavs-Laptop-5.local>
2025-01-12 02:26:30 +0530

    Updated basic_frontend.py to add markup hyperlinks and handle CSV input

--- basic_frontend.py
+++ basic_frontend.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import altair as alt
 import pandas as pd
-
 st.set_page_config(page_title="EventConnect", page_icon="📅", layout="wide")
+
+"""
+input: csv1(priority sorted) is df1 and csv2(time sorted) is df2 and d
+output: calendar
+extra need: all csv time in that format..... 24 hour format
+"""
+
+
 
 # Center-align the title using HTML and CSS
 st.markdown(
@@ -32,7 +39,7 @@ def create_block(vertical_lines, links):
 
     # Create an Altair chart
     chart = alt.Chart(data).mark_rule(color='red').encode(
-        x=alt.X('x:Q', scale=alt.Scale(domain=[0, 1])),  # Limit the x-axis range
+        x=alt.X('x:Q', scale=alt.Scale(domain=[7, 23]), title='Time (hour)'),  # Rename x-axis
         tooltip=['link:N']
     ).properties(
         width=100,
@@ -57,12 +64,10 @@ def create_block(vertical_lines, links):
         baseline='bottom',
         fontSize=12,
         color='blue',
-        dy=-10
+        dy=10
     ).encode(
         x='x:Q',
         href='link:N'
-    ).transform_calculate(
-        href='datum.link'
     )
 
     # Combine the chart and text
@@ -70,53 +75,87 @@ def create_block(vertical_lines, links):
     return final_chart
 
 # Example vertical lines and links for multiple blocks
-vertical_lines1 = [0.1, 0.3, 0.5, 0.7, 0.9]
-links1 = [
-    "https://www.example.com/link1",
-    "https://www.example.com/link2",
-    "https://www.example.com/link3",
-    "https://www.example.com/link4",
-    "https://www.example.com/link5"
-]
 
-vertical_lines2 = [0.2, 0.4, 0.6, 0.8]
-links2 = [
-    "https://www.example.com/link6",
-    "https://www.example.com/link7",
-    "https://www.example.com/link8",
-    "https://www.example.com/link9"
-]
 
-vertical_lines3 = [0.2, 0.4, 0.6, 0.8]
-links3 = [
-    "https://www.example.com/link10",
-    "https://www.example.com/link11",
-    "https://www.example.com/link12",
-    "https://www.example.com/link13"
-]
+df1 = pd.read_csv("priority_sorted.csv")
+# df2 = pd.read_csv("time_sorted.csv")
+index=df1['Priority index'][0]
+vertical_lines=[]
+links=[]
+texted=[]
+for i in range(len(df1)):
+    if df1['Priority index'][i]==index:
+        vertical_lines.append(df1['Time'][i])
+        links.append(df1['Link'][i])
+        texted.append(df1['Description'][i])
+    else:
+        with st.container():
+            left,right =st.columns([4, 1])
+            with left:
+                block = create_block(vertical_lines, links)
+                st.altair_chart(block, use_container_width=True)
+            with right:
+                st.write(df1['Type'][i-1])
+                for i in range(len(texted)):
+                    st.write(1,". [{texted[i]}]({links[i]})")
 
-# Create multiple blocks
-with st.container():
-    left,right =st.columns([4, 1])
-    with left:
-        block1 = create_block(vertical_lines1, links1)
-        st.altair_chart(block1, use_container_width=True)
-    with right:
-        st.write("asdf")
 
-with st.container():
-    left,right =st.columns([4, 1])
-    with left:
-        block2 = create_block(vertical_lines2, links2)
-        st.altair_chart(block2, use_container_width=True)
-    with right:
-        st.write("asdf")
+df2=pd.read_csv("time_sorted.csv")
+#Here to write last 2 special things
 
-with st.container():
-    left,right =st.columns([4, 1])
-    with left:
-        block3 = create_block(vertical_lines3, links3)
-        st.altair_chart(block3, use_container_width=True)
-    with right:
-        st.write("asdf")
+
+                
+
+
+
+
+# vertical_lines1 = [0.1, 0.3, 0.5, 0.7, 0.9]
+# links1 = [
+#     "https://www.example.com/link1",
+#     "https://www.example.com/link2",
+#     "https://www.example.com/link3",
+#     "https://www.example.com/link4",
+#     "https://www.example.com/link5"
+# ]
+
+# vertical_lines2 = [0.2, 0.4, 0.6, 0.8]
+# links2 = [
+#     "https://www.example.com/link6",
+#     "https://www.example.com/link7",
+#     "https://www.example.com/link8",
+#     "https://www.example.com/link9"
+# ]
+
+# vertical_lines3 = [0.2, 0.4, 0.6, 0.8]
+# links3 = [
+#     "https://www.example.com/link10",
+#     "https://www.example.com/link11",
+#     "https://www.example.com/link12",
+#     "https://www.example.com/link13"
+# ]
+
+# # Create multiple blocks
+# with st.container():
+#     left,right =st.columns([4, 1])
+#     with left:
+#         block1 = create_block(vertical_lines1, links1)
+#         st.altair_chart(block1, use_container_width=True)
+#     with right:
+#         st.write("asdf")
+
+# with st.container():
+#     left,right =st.columns([4, 1])
+#     with left:
+#         block2 = create_block(vertical_lines2, links2)
+#         st.altair_chart(block2, use_container_width=True)
+#     with right:
+#         st.write("asdf")
+
+# with st.container():
+#     left,right =st.columns([4, 1])
+#     with left:
+#         block3 = create_block(vertical_lines3, links3)
+#         st.altair_chart(block3, use_container_width=True)
+#     with right:
+#         st.write("asdf")
 
